07242f8e7fd6bb4e85676bffd9fc1f6814621f7d
John Watson <john.watson2@rpa.gov.uk>
2020-01-27 19:38:51 +0000

    Environment variables updated for SQS

--- FFCDemoPaymentService/Messaging/MesssageConfig.cs
+++ FFCDemoPaymentService/Messaging/MesssageConfig.cs
@@ -5,11 +5,19 @@ namespace FFCDemoPaymentService.Messaging
 {
     public class MessageConfig
     {
-        public string Host { get; set; }
-        public int Port { get; set; }
-        public bool UseSsl { get; set; }
-        public string PaymentQueue { get; set; }
-        public string PaymentUserName { get; set; }
-        public string PaymentPassword { get; set; }
+        public string ScheduleQueueName { get; set; }
+        public string ScheduleQueueEndpoint { get; set; }
+        public string ScheduleQueueUrl { get; set; }
+        public string ScheduleQueueRegion { get; set; }
+        public string ScheduleAccessKeyId { get; set; }
+        public string ScheduleAccessKey { get; set; }
+        public bool CreateScheduleQueue { get; set; }
+        public string PaymentQueueName { get; set; }
+        public string PaymentQueueEndpoint { get; set; }
+        public string PaymentQueueUrl { get; set; }
+        public string PaymentQueueRegion { get; set; }
+        public string PaymentAccessKeyId { get; set; }
+        public string PaymentAccessKey { get; set; }
+        public bool CreatePaymentQueue { get; set; }
     }
 }
