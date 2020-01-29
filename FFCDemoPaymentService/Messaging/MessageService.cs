2eccd71fd44d80abbef5948c859b9d9a7a8137ee
John Watson <john.watson2@rpa.gov.uk>
2020-01-29 20:20:07 +0000

    Console logs added

--- FFCDemoPaymentService/Messaging/MessageService.cs
+++ FFCDemoPaymentService/Messaging/MessageService.cs
@@ -24,31 +24,25 @@ namespace FFCDemoPaymentService.Messaging
 
         public void StartPolling()
         {
-            List<Task> tasks = new List<Task>();
-
             var scheduleReceiver = new SqsReceiver();
-            var t1 = Task.Run(() => scheduleReceiver.StartPolling(messageConfig.ScheduleQueueEndpoint, 
-                messageConfig.ScheduleQueueUrl, 
-                new Action<string>(ScheduleHandler), 
-                messageConfig.ScheduleAccessKeyId, 
+            var t1 = Task.Run(() => scheduleReceiver.StartPolling(messageConfig.ScheduleQueueEndpoint,
+                messageConfig.ScheduleQueueUrl,
+                new Action<string>(ScheduleHandler),
+                messageConfig.ScheduleAccessKeyId,
                 messageConfig.ScheduleAccessKey,
                 messageConfig.CreateScheduleQueue,
                 messageConfig.ScheduleQueueName
                 ));
-            tasks.Add(t1);
 
             var paymentReceiver = new SqsReceiver();
-            var t2 = Task.Run(() => paymentReceiver.StartPolling(messageConfig.PaymentQueueEndpoint, 
-                messageConfig.PaymentQueueUrl, 
-                new Action<string>(PaymentHandler), 
-                messageConfig.PaymentAccessKeyId, 
+            var t2 = Task.Run(() => paymentReceiver.StartPolling(messageConfig.PaymentQueueEndpoint,
+                messageConfig.PaymentQueueUrl,
+                new Action<string>(PaymentHandler),
+                messageConfig.PaymentAccessKeyId,
                 messageConfig.PaymentAccessKey,
                 messageConfig.CreatePaymentQueue,
-                messageConfig.PaymentQueueName                
+                messageConfig.PaymentQueueName
                 ));
-            tasks.Add(t2);
-
-            Task.WaitAll(tasks.ToArray());
         }
 
         public void ScheduleHandler(string message)
--- FFCDemoPaymentService/Messaging/SqsReceiver.cs
+++ FFCDemoPaymentService/Messaging/SqsReceiver.cs
@@ -9,29 +9,40 @@ namespace FFCDemoPaymentService.Messaging
 {
     public class SqsReceiver
     {
-        public async Task StartPolling(string endpoint, string queueUrl, Action<string> messageAction, string keyId, string key, bool createQueue, string QueueName)
+        public async Task StartPolling(string endpoint, string queueUrl, Action<string> messageAction, string keyId, string key, bool createQueue, string queueName)
         {
+            Console.WriteLine("Configuring queue {0}", queueName);
+            Console.WriteLine("#############################");
+
             var awsCredentials = new Amazon.Runtime.BasicAWSCredentials(keyId, key);
+            Console.WriteLine("Credentials {0}", awsCredentials);
 
             AmazonSQSConfig amazonSQSConfig = new AmazonSQSConfig();
             amazonSQSConfig.ServiceURL = endpoint;
+
 
             var amazonSQSClient = new AmazonSQSClient(awsCredentials, amazonSQSConfig);
 
             if (createQueue)
             {
+                Console.WriteLine("Creating queue {0}", queueName);
+                Console.WriteLine("#############################");
+
                 CreateQueueRequest createQueueRequest = new CreateQueueRequest();
-                createQueueRequest.QueueName = QueueName;
+                createQueueRequest.QueueName = queueName;
 
                 CreateQueueResponse createQueueResponse = await amazonSQSClient.CreateQueueAsync(createQueueRequest);
             }
+
+            Console.WriteLine("Ready to receive message from {0}", queueName);
+            Console.WriteLine("#############################");
 
             // receive a message
             ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest();
             receiveMessageRequest.QueueUrl = queueUrl;
 
-            // while (true)
-            // {
+            while (true)
+            {
                 ReceiveMessageResponse receiveMessageResponse = await amazonSQSClient.ReceiveMessageAsync(receiveMessageRequest);
 
                 if (receiveMessageResponse.Messages.Count > 0)
@@ -40,6 +51,9 @@ namespace FFCDemoPaymentService.Messaging
 
                     try
                     {
+                        Console.WriteLine("Received message");
+                        Console.WriteLine("#############################");
+
                         messageAction(receiveMessageResponse.Messages[0].Body);
 
                         // delete message
@@ -55,8 +69,7 @@ namespace FFCDemoPaymentService.Messaging
                         Console.WriteLine("Unable to process message", ex);
                     }
                 }
-
-            // }
+            }
         }
     }
 }
