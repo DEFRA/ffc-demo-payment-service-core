723df78337d804e2361a1880212a607cebd59bea
John Watson <john.watson2@rpa.gov.uk>
2020-01-30 08:24:02 +0000

    Receiver removed

--- FFCDemoPaymentService/Startup.cs
+++ FFCDemoPaymentService/Startup.cs
@@ -33,9 +33,6 @@ namespace FFCDemoPaymentService
             services.AddControllers();
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
-
-            // var sender = new SqsSender();
-            // Task.Run(() => sender.SendMessage(messageConfig));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
