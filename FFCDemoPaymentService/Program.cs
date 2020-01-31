d0b65627e40ec286f8246fbc5c6e81523ff998b0
John Watson <john.watson2@rpa.gov.uk>
2020-01-31 14:10:14 +0000

    Messaging restored

--- FFCDemoPaymentService/Program.cs
+++ FFCDemoPaymentService/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using FFCDemoPaymentService.Messaging;
 
 namespace FFCDemoPaymentService
 {
@@ -23,6 +24,10 @@ namespace FFCDemoPaymentService
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<MessageService>();
                 });
     }
 }
