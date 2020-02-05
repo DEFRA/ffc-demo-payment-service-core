91f705417305c458bc59526cbf5c40906a4e73e2
John Watson <john.watson2@rpa.gov.uk>
2020-02-05 13:11:58 +0000

    Program and Startup refactored

--- FFCDemoPaymentService/Startup.cs
+++ FFCDemoPaymentService/Startup.cs
@@ -1,13 +1,10 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using FFCDemoPaymentService.Data;
 using FFCDemoPaymentService.Messaging;
 using FFCDemoPaymentService.Messaging.Actions;
