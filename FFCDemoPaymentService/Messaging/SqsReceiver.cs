e6717926a97187b066796012e0d020c3a4d74d68
John Watson <john.watson2@rpa.gov.uk>
2020-02-08 17:04:47 +0000

    Creds changed

--- FFCDemoPaymentService/Messaging/SqsReceiver.cs
+++ FFCDemoPaymentService/Messaging/SqsReceiver.cs
@@ -14,7 +14,7 @@ namespace FFCDemoPaymentService.Messaging
     public class SqsReceiver : IReceiver
     {
         readonly SqsConfig sqsConfig;
-        BasicAWSCredentials credentials;
+        SessionAWSCredentials credentials;
         AmazonSQSConfig amazonSQSConfig;
         AmazonSQSClient amazonSQSClient;
         readonly Action<string> messageAction;
