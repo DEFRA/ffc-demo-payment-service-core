f51e523c53edfd81b305b057fb3e4b813358f68d
John Watson <johnwatson484@hotmail.com>
2020-07-20 17:56:45 +0100

    Port references removed

--- FFCDemoPaymentService/Messaging/MesssageConfig.cs
+++ FFCDemoPaymentService/Messaging/MesssageConfig.cs
@@ -6,7 +6,6 @@ namespace FFCDemoPaymentService.Messaging
         public string ScheduleQueueName { get; set; }
         public string PaymentQueueName { get; set; }
         public string MessageQueueHost { get; set; }
-        public string MessageQueuePort { get; set; }
         public string MessageQueuePreFetch { get; set; }
         public string MessageQueueUser { get; set; }
         public string MessageQueuePassword { get; set; }
