7dd10e0bc673ce60c700e58f75a1933fd6e0738a
Paul Andrews <paul.andrews@defra.gov.uk>
2020-07-27 20:50:28 +0100

    testing

--- FFCDemoPaymentService/Startup.cs
+++ FFCDemoPaymentService/Startup.cs
@@ -33,6 +33,8 @@ namespace FFCDemoPaymentService
             AddTelemetry(services);
 
             var defaultConnectionString = Configuration.GetConnectionString("DefaultConnection");
+            Console.WriteLine($"DEFAULT Connection String: {defaultConnectionString}");
+
             var builder = new ConnectionStringBuilder(defaultConnectionString);
             string connStr = Task.Run(builder.GetConnectionString).Result;
 
