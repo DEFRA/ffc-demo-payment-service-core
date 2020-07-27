bd033d895118f97361076c3f7106d1a8b83cae19
Paul Andrews <paul.andrews@defra.gov.uk>
2020-07-27 19:51:33 +0100

    testing 2

--- FFCDemoPaymentService/Startup.cs
+++ FFCDemoPaymentService/Startup.cs
@@ -90,7 +90,7 @@ namespace FFCDemoPaymentService
                 endpoints.MapControllers();
             });
 
-            ApplyMigrations(dbContext);
+            //ApplyMigrations(dbContext);
         }
 
         public void ApplyMigrations(ApplicationDbContext dbContext)
