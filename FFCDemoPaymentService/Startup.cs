6d58319786dada5ce6c05f4bf5efe3b5af8a9a07
John Watson <johnwatson484@hotmail.com>
2021-01-30 14:32:41 +0000

    Remove redundant DI

--- FFCDemoPaymentService/Startup.cs
+++ FFCDemoPaymentService/Startup.cs
@@ -73,7 +73,7 @@ namespace FFCDemoPaymentService
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext dbContext)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
